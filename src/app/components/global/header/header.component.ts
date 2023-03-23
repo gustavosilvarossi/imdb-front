8e7fb7f1cbf3eedb5fe4db9a32ad5d92cb09fa4d
gustavosrossi <gustavo.rossi@outlook.com.br>
2023-03-22 21:27:45 -0300

    feat: add tratamento para header

--- src/app/components/global/header/header.component.ts
+++ src/app/components/global/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/global/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authSrv: AuthService) { }
 
   ngOnInit(): void {
-   
+
   }
 
 }
